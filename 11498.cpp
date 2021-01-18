74c314417a0bd7d6d44a9d14e93dc67ebc752bdf
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-01-19 00:35:07 +0530

    11498 - Division of Nlogonia

---
+++ 11498.cpp
@@ -0,0 +1,60 @@
+/*
+ ~ Author    : tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define pb                  push_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+int main() {
+
+	FIO;
+
+	int k;
+
+	while (cin >> k) {
+
+		int n, m;
+		cin >> n >> m;
+
+		while (k--) {
+
+			int x, y;
+			cin >> x >> y;
+
+			if (x == n || y == m)
+				cout << "divisa" << '\n';
+			else if (x < n && y > m)
+				cout << "NO" << '\n';
+			else if (x > n && y > m)
+				cout << "NE" << '\n';
+			else if (x > n && y < m)
+				cout << "SE" << '\n';
+			else if (x < n && y < m)
+				cout << "SO" << '\n';
+
+		}
+	}
+
+	return 0;
+}
