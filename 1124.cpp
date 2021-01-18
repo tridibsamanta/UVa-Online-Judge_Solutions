e81daa56235f4e01f2512eef50dec1fc720e30e1
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-01-19 00:34:04 +0530

    1124 - Celebrity jeopardy

---
+++ 1124.cpp
@@ -0,0 +1,17 @@
+/*
+ ~ Author    : tridib_2003
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+	string str;
+
+	while (getline(cin, str))
+		cout << str << '\n';
+
+	return 0;
+}
